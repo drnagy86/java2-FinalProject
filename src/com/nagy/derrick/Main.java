7bbdb166f3998e871f386efc64d8564c788853ab
Derrick Nagy <drnagy86@gmail.com>
2021-08-25 14:18:07 -0500

    initial commit

---
+++ src/com/nagy/derrick/Main.java
@@ -0,0 +1,36 @@
+package com.nagy.derrick;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Book[] bookshelf = new Book[4];
+        bookshelf[0] = new Book(
+                "Python",
+                "Marc Hauschildt",
+                false,
+                1000
+        );
+        bookshelf[1] = new Book(
+                "Java",
+                "Marc Hauschildt",
+                false,
+                750
+        );
+        bookshelf[2] = new Book(
+                "Java",
+                "Ada Lovelace",
+                false,
+                500
+        );
+        bookshelf[3] = new Book();
+
+        Arrays.sort(bookshelf);
+        System.out.println("There are " + Book.getBookCount() + " books on the bookshelf.");
+        for (Book book : bookshelf){
+            System.out.println(book);
+        }
+
+    }
+}
