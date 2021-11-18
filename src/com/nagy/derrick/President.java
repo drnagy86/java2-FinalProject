999fe71c7dcc043aa7a848ff66fa76f90060e641
Derrick Nagy <derrick-nagy@student.kirkwood.edu>
2021-11-17 21:18:21 -0600

    add President tests

--- src/com/nagy/derrick/President.java
+++ src/com/nagy/derrick/President.java
@@ -4,39 +4,68 @@ import java.time.LocalDate;
 
 public class President extends Person implements Comparable<President>{
 
-    private int inaugurationAge_Years;
-    private int inaugurationAge_Days;
+    private int inaugurationAgeYears;
+    private int inaugurationAgeDays;
 
-
-    public static final int DEFAULT_INAUGURATION_AGE_YEARS = 4;
-    public static final int DEFAULT_INAUGURATION_AGE_DAYS = 1;
+    public static final int DEFAULT_INAUGURATION_AGE_YEARS = 40;
+    public static final int DEFAULT_INAUGURATION_AGE_DAYS = 200;
 
     public static final int MIN_INAUGURATION_AGE_YEARS = 35;
     public static final int MAX_INAUGURATION_AGE_YEARS = 100;
     public static final int MIN_INAUGURATION_AGE_DAYS = 1;
     public static final int MAX_INAUGURATION_AGE_DAYS = 365;
 
-    public static final String ILLEGAL_ARGUEMENT_EXCEPTION_MESSAGE_IAUGURATION_AGE_YEARS = "Age must be between 35 and 100 years old.";
-    public static final String ILLEGAL_ARGUEMENT_EXCEPTION_MESSAGE_IAUGURATION_AGE_DAYS = "Days must be between 1 and 365.";
+    public static final String MSG_ILLEGAL_ARGUMENT_EXCEPTION_INAUGURATION_AGE_YEARS = "Age must be between 35 and 100 years old.";
+    public static final String MSG_ILLEGAL_ARGUMENT_EXCEPTION_INAUGURATION_AGE_DAYS = "Days must be between 1 and 365.";
 
     public President() {
         super(DEFAULT_FIRST_NAME, DEFAULT_LAST_NAME, DEFAULT_BIRTHDAY);
-        this.inaugurationAge_Years = DEFAULT_INAUGURATION_AGE_YEARS;
-        this.inaugurationAge_Days =DEFAULT_INAUGURATION_AGE_DAYS;
+        this.inaugurationAgeYears = DEFAULT_INAUGURATION_AGE_YEARS;
+        this.inaugurationAgeDays =DEFAULT_INAUGURATION_AGE_DAYS;
     }
 
-    public President(String firstName, String lastName, LocalDate birthday, int inaugurationAge_Years, int inaugurationAge_Days) {
+    public President(String firstName, String lastName, LocalDate birthday, int inaugurationAgeYears, int inaugurationAgeDays) {
         super(firstName, lastName, birthday);
-        this.inaugurationAge_Years = inaugurationAge_Years;
-        this.inaugurationAge_Days = inaugurationAge_Days;
+        validateInaugurationAgeYears(inaugurationAgeYears);
+        validateInaugurationAgeDays(inaugurationAgeDays);
+
+        this.inaugurationAgeYears = inaugurationAgeYears;
+        this.inaugurationAgeDays = inaugurationAgeDays;
     }
+
+    public int getInaugurationAgeYears() {
+        return inaugurationAgeYears;
+    }
+
+    public void setInaugurationAgeYears(int inaugurationAgeYears) {
+        validateInaugurationAgeYears(inaugurationAgeYears);
+        this.inaugurationAgeYears = inaugurationAgeYears;
+    }
+
+    public int getInaugurationAgeDays() {
+        return inaugurationAgeDays;
+    }
+
+    public void setInaugurationAgeDays(int inaugurationAgeDays) {
+        validateInaugurationAgeDays(inaugurationAgeDays);
+        this.inaugurationAgeDays = inaugurationAgeDays;
+    }
+
+    public void validateInaugurationAgeYears(int years){
+        if (years > MAX_INAUGURATION_AGE_YEARS || years < MIN_INAUGURATION_AGE_YEARS ) throw new IllegalArgumentException(MSG_ILLEGAL_ARGUMENT_EXCEPTION_INAUGURATION_AGE_YEARS);
+    }
+
+    public void validateInaugurationAgeDays(int days){
+        if (days > MAX_INAUGURATION_AGE_DAYS || days < MIN_INAUGURATION_AGE_DAYS) throw new IllegalArgumentException(MSG_ILLEGAL_ARGUMENT_EXCEPTION_INAUGURATION_AGE_DAYS);
+    }
+
 
     @Override
     public String toString() {
         return super.toString() +
                 "President{" +
-                "inaugurationAge_Years=" + inaugurationAge_Years +
-                ", inaugurationAge_Days=" + inaugurationAge_Days +
+                "inaugurationAge_Years=" + inaugurationAgeYears +
+                ", inaugurationAge_Days=" + inaugurationAgeDays +
                 '}';
     }
 
@@ -46,18 +75,15 @@ public class President extends Person implements Comparable<President>{
         // second by "inaugurationAge_Days" in descending order,
         // and third by "lastName" is ascending order.
 
-        int result = (this.inaugurationAge_Years - other.inaugurationAge_Years) * -1;
+        int result = (this.inaugurationAgeYears - other.inaugurationAgeYears) * -1;
 
         if (result == 0){
-            result = (this.inaugurationAge_Days - other.inaugurationAge_Days) * -1;
+            result = (this.inaugurationAgeDays - other.inaugurationAgeDays) * -1;
 
             if (result == 0){
                 result = (this.getLastName().compareToIgnoreCase(other.getLastName()));
             }
         }
-
-
-
         return result;
     }
 }
